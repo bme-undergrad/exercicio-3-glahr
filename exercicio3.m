1faf96fd87b58fbd6c3d8554c67e7b73b13fd092
Gustavo Lahr <gjgl.lahr@gmail.com>
2025-10-20 10:59:26 -0300

    Update mu calculation based on polyfit result
    Calculate mu using the slope from polyfit.

--- exercicio3.m
+++ exercicio3.m
@@ -7,9 +7,18 @@ function [mu] = exercicio3(t, NC)
 
 % digite seu codigo aqui
 
+t = t - t(1);
+NC_ln = log(NC);
+
+a = polyfit(t, NC_ln, 1)
+
+a1 = a(1);
+a0 = a(2);
+
+x0 = exp(a0);
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
+mu = a1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
